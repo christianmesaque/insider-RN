ccf33bfcbfffda6b5384b8a7a02ef8e791369e50
Christian Mesaque <you@example.com>
2022-06-01 23:01:17 -0300

    1.0

---
+++ src/routes.js
@@ -0,0 +1,61 @@
+
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Home from './pages/home'
+import Detail from './pages/detail'
+import CategoryPosts from './pages/categoryPosts'
+import Search from './pages/search'
+
+const Stack = createNativeStackNavigator();
+
+function Routes(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false
+        }}
+      />
+
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{
+          title: 'Detalhes',
+          headerTintColor: '#fff',
+          headerStyle:{
+            backgroundColor: '#232630'
+          }
+        }}
+      />
+
+      <Stack.Screen
+        name="Category"
+        component={CategoryPosts}
+        options={{
+          headerTintColor: '#fff',
+          headerStyle:{
+            backgroundColor: '#232630'
+          }
+        }}
+      />
+
+      <Stack.Screen
+        name="Search"
+        component={Search}
+        options={{
+          title: 'Procurando algo?',
+          headerTintColor: '#fff',
+          headerStyle:{
+            backgroundColor: '#232630'
+          }
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
+
+export default Routes;
